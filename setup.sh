653f3fd38f999f564bf0b2121fbaeca8b1eb2bcb
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-06-18 00:40:12 +0900

    fix: ディレクトリ初期ファイルの追加。一部モデルの移動

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #/bin/sh
-mkdir models analysis tests seeds macros snapshots target models/materialized_dwh
+mkdir models analysis tests seeds macros snapshots target models/dwh models/stg
+
+touch models/.gitkeep
+touch models/dwh/.gitkeep
+touch models/stg/.gitkeep
+touch analysis/.gitkeep 
+touch tests/.gitkeep
+touch seeds/.gitkeep
+touch macros/.gitkeep
+touch snapshots/.gitkeep
